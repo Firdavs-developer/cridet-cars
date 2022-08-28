10f3d70c05e36186a6faa330f686e779188b66bd
Firdavs-developer <firdavsdosiyorov3@gmail.com>
2022-08-28 10:02:58 +0500

    inestall-comment

---
+++ javaScript.js
@@ -0,0 +1,57 @@
+// // task 1
+// let a = 14
+// for (i = 1; i<=a; i++) {
+//     if (i%2==0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// task 2
+let arr1 = [1,3,5,7,9]
+let arr2 = [2,4,6,8,10]
+let a = 0
+let b = 0
+for (i of arr1) { a+=i }
+for (i of arr2) { b+=i }
+console.log(a+b);
+
+
+
+// // task 3
+// let str = "Yes true"
+// letter = false
+// for (i of str) {
+//     if(i == "y") {
+//         letter = true
+//         break
+//     }
+// }
+// if (letter) {console.log("yes")}
+// else (console.log("Yoq"))
+
+
+
+// // task 4
+// let str="amma"
+// let arr=[]
+// let palindrom=true
+// for (let i of str) {
+//     arr.push(i)
+// }
+// for (i = 0; i<arr.length/2; i++) {
+//     if (arr[i] != arr[arr.length - i-1]) {
+//         palindrom = false
+//         break;
+//     }
+// }
+// if(palindrom==false) console.log("No");
+// else console.log("Yes");
+
+
+// // task 5
+// let a = 10 
+// let b = 0
+// for ( i=0; i<=a; i++ ){b+=i} 
+// console.log(b);
